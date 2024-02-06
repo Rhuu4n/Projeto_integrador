6653683931924860059f58b22110a3f0095ea4d9
Hugo Henrique <imhugooooo@gmail.com>
2024-02-06 20:23:45 -0300

    puxando as funções PARTIDA

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from controllers.controllers_users import get_users, set_users, del_users
 from controllers.controllets_partida import set_matches, get_matches, put_matches
+from controllers_rooms.py import create_room, get_rooms
 
 app = Flask(__name__)
 
@@ -8,11 +9,14 @@ app = Flask(__name__)
 app.route("/users", methods=["POST"])(set_users)
 app.route("/users", methods = ["GET"])(get_users)
 app.route("/users/<int:id>", methods = ["DELETE"])(del_users)
-@app.route('/players/<int:player_id>', methods=['GET'])(get_player)
+app.route('/players/<int:player_id>', methods=['GET'])(get_player)
 
 # Create, Read, Update do objeto partida
 app.route("/matches", methods=["POST"])(set_matches)
 app.route("/matches", methods=["GET"])(get_matches)
 app.route("/matches/<int:id>", methods=["PUT"])(get_matches)
+
+# Create, Read, Update Das informações da partida
+app.route('/create_room', methods=['POST'])(create_room) 
 
 app.run(debug=True)
--- controllers/controller_rooms.py
+++ controllers/controller_rooms.py
@@ -1,3 +1,8 @@
+from flask import Flask, request, jsonify
+
+
+
+
 rooms = [
     {
         "room_id":555,
