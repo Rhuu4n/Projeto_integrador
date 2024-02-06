bc981e79eaec81dd25672ce19b6861d365d0f661
Erick Rosdachimo <erickrosdachimoribeiro@gmail.com>
2024-02-05 21:31:11 -0300

    Adicionando Get e Delete no c_user

--- CRD_User.py
+++ CRD_User.py
@@ -20,6 +20,28 @@ users = [
      }
 ]
 
+@app.route("/users", methods = ["POST"])
+def crt_users():
+    New_user = request.get_json()
+
+    for u in users:
+        if New_user["id"] == u["id"]:
+            return jsonify({"erro:":"id ja existente"}), 409
+         
+        if New_user["usuario"] == u["usuario"] or New_user["usuario"] == "":
+            return jsonify({"erro:":"usuario ja existente ou vazio"}), 409
+        
+    if len(New_user["senha"]) >= 9:
+        return jsonify({"erro":"senha longa demais"})
+        
+    users.append(New_user)
+    return jsonify(users)
+
+
+@app.route("/users", methods = ["GET"])
+def get_users():
+    return users
+
 @app.route("/users/<int:id>", methods = ["DELETE"])
 def del_aluno(id):
     
@@ -27,5 +49,6 @@ def del_aluno(id):
           if id == u["id"]:
                del users[i]
                return jsonify(users)
-          
+
+
 app.run(debug=True)
