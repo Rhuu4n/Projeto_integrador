6c51550f0dd23815f4dfdf3489b187ef548b96f7
VmcMK <rafinha.yamamoto75@gmail.com>
2024-02-06 21:26:34 -0300

    sala partida

--- controllers/controller_rooms.py
+++ controllers/controller_rooms.py
@@ -1,6 +1,6 @@
 rooms = [
     {
-        "room_id":555,
+        "id":555,
         "status_room":0,
         "player_1_id": 1,
         "player_2_id": 2,
--- victor_teste.py
+++ victor_teste.py
@@ -1,5 +1,5 @@
 from flask import Flask,jsonify
-from controllers.controller_rooms import get_rooms
+from controllers.controller_rooms import get_rooms,rooms
 
 app = Flask(__name__)
 
@@ -8,11 +8,22 @@ app.route("/rooms", methods=["GET"])(get_rooms)
 
 @app.route("/rooms/<int:id>", methods= ["GET"])
 def get_rooms_by_id(id):
-    for i, get_rooms in enumerate (get_rooms):
-        if id == get_rooms_by_id ["id"]:
-            return jsonify(get_rooms[i])
+    for i, room in enumerate (rooms):
+        if id == room["id"]:
+            return jsonify(rooms[i])
 
 
-    return jsonify({"Sala Não Encontrada"})
+    return jsonify({"Erro":"Sala Nao Encontrada"}),404
 
 app.run(debug=True)
+
+
+@app.route('/delete_match/<int:room_id>', methods=['DELETE'])
+def delete_room (room_id):
+
+    for room in rooms:
+        if room['room_id'] == room_id:
+            rooms.remove(room)
+            return jsonify({'mensagem': 'sala deletada com sucesso'}), 200
+        
+    return({"error": "Partida Nao Encontrada"}), 404
