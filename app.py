df863094031876c12af1277e3ed5b9def9593efe
Hugo Henrique <imhugooooo@gmail.com>
2024-02-07 20:18:08 -0300

    criando pasta routes e adicionando rota users

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, request, jsonify
 
-from controllers.controllers_users import get_users, set_users, del_users, get_user, put_user
 from controllers.controllers_partida import set_matches, get_matches, put_matches, get_matches_by_id
 from controllers.controller_rooms import create_room, get_rooms, delete_room, put_room
 
 app = Flask(__name__)
 
-# Create, Read e Delete do objeto users
-app.route("/users", methods=["POST"])(set_users)
-app.route("/users", methods = ["GET"])(get_users)
-app.route("/users/<int:id>", methods = ["DELETE"])(del_users)
-app.route("/users/<int:id>", methods = ["PUT"])(put_user)
-app.route('/users/<int:user_id>', methods=['GET'])(get_user)
+
+app.register_blueprint(bp_users)
 
 # Create, Read, Update do objeto partida
 app.route("/matches", methods=["POST"])(set_matches)
