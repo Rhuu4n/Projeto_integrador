c4043578aef9bfd144923387f006866160cf4da9
Danilo <dan.conceicao03@gmail.com>
2024-03-27 22:09:39 -0300

    MYSQL atualizado, app.py, Model cadastro(user)

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, request, jsonify
-from routes.routes_users import bp_users
-from routes.routes_matches import bp_matches
-from routes.routes_rooms import bp_rooms
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-app.register_blueprint(bp_users)
-app.register_blueprint(bp_matches)
-app.register_blueprint(bp_rooms)
+def create_app():
+    from routes.routes_users import bp_users
+    from routes.routes_matches import bp_matches
+    from routes.routes_rooms import bp_rooms
 
-app.run(debug=True)
+    app = Flask(__name__)
+
+    app.register_blueprint(bp_users)
+    app.register_blueprint(bp_matches)
+    app.register_blueprint(bp_rooms) 
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://projeto:Ubuntu12!@10.60.46.21/Projeto'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    return app
+    # -h ip  -u -p 
