4b058323798759cda5b749e2a225a079bd9f2b19
Rhuan Oliveira~ <Rhuan.padua@hotmail.com>
2024-04-02 20:23:36 -0300

    put_rooms criado vetor deletado

--- controllers/controller_rooms.py
+++ controllers/controller_rooms.py
@@ -2,17 +2,6 @@ from flask import Flask, request, jsonify
 from models.models_rooms import Salas
 from app import db
 
-rooms = [
-    {
-        "id":555,
-        "status_room":0,
-        "player_1_id": 1,
-        "player_2_id": 2,
-        "player_3_id": 3,
-        "player_4_id": 4
-    }
-]
-
 def create_room():
     new_room = request.get_json()
 
@@ -20,31 +9,35 @@ def create_room():
     db.session.add(crt_room)
     db.session.commit()
         
-    return jsonify(room.to_json()), 201    
+    return jsonify(crt_room.to_json()), 201    
     
 def get_rooms():
-    return jsonify(rooms)
+    rooms =  Salas.query.all()
 
+    return [c.to_json() for c in rooms]
 
 def delete_room (room_id):
     
     del_room = db.get_or_404(Salas, id)
     db.session.delete(del_room)
-
     db.session.commit()
 
     return jsonify({"Excluido":"Evento foi deletado com sucesso"}), 200
 
 
 def put_room(id):
-     update_room = request.get_json()
 
-     for i, room in enumerate(rooms):
-        if id == room["id"]:
-             rooms[i].update(update_room)
-             return jsonify(room)
-            
-     return jsonify({"erro":"user nao encontrado"}), 404
+    put_room = request.get_json()
+
+    p_room = db.get_or_404(Salas, id)
+
+    p_room.jogadorAtual = put_room.get("jogadorAtual")
+    p_room.estadoSala = put_room.get("estadoSala")
+    p_room.numeroJogadores = put_room.get("numeroJogadores")
+    
+    db.session.commit()
+
+    return jsonify(p_room.to_json()), 201
 
 def get_rooms_by_id(id):
     for i, room in enumerate (rooms):
