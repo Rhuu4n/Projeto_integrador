d762ea38527aa56dfd55bf4ea2f460aa252c9166
Erick Rosdachimo <erickrosdachimoribeiro@gmail.com>
2024-04-16 20:20:44 -0300

    arrumando rotas rooms

--- routes/routes_rooms.py
+++ routes/routes_rooms.py
@@ -3,9 +3,8 @@ from controllers.controller_rooms import create_room, get_rooms, delete_room, pu
 
 bp_rooms = Blueprint('bp_rooms', __name__)
 
-bp_rooms.route('/create_room', methods=['POST'])(create_room)
-bp_rooms.route('/create_room', methods=['GET'])(get_rooms) 
-bp_rooms.route('/delete_room/<int:room_id>', methods=['DELETE'])(delete_room)
-bp_rooms.route('/rooms/<int:id>', methods=['PUT'])(put_room)
+bp_rooms.route('/rooms', methods=['POST'])(create_room)
 bp_rooms.route('/rooms', methods=['GET'])(get_rooms) 
+bp_rooms.route('/rooms/<int:room_id>', methods=['DELETE'])(delete_room)
+bp_rooms.route('/rooms/<int:id>', methods=['PUT'])(put_room)
 bp_rooms.route('/rooms/<int:id>', methods=['GET'])(get_rooms_by_id)
